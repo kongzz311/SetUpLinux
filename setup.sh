81d5b493fc13fa7bc65a965391b9543dc7ef3456
kongzz311 <33396281+kongzz311@users.noreply.github.com>
2020-12-06 14:14:07 +0800

    .

--- setup.sh
+++ setup.sh
@@ -18,6 +18,28 @@ sudo apt-get install -y python-dev python-pip python3-dev
 sudo apt-get install -y python3-setuptools
 sudo easy_install3 pip
 
+# Install neovim
+sudo add-apt-repository ppa:neovim-ppa/stable
+sudo apt-get update
+sudo apt-get install -y neovim
+
+
+# Install vim setup dependencies
+python2 -m pip install --user --upgrade pynvim
+python3 -m pip install --user --upgrade pynvim
+npm install -g neovim
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+cargo install bat
+cargo install devicon-lookup
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+sudo apt-get install -y silversearcher-ag
+
+
 # Install Node.js
 curl -L https://raw.githubusercontent.com/tj/n/master/bin/n -o n
 sudo bash n lts
@@ -26,8 +48,6 @@ mkdir ~/.npm-global
 npm config set prefix '~/.npm-global'
 npm install -g n
 
-# Install redis
-sudo apt-get install -y redis-server
 
 # Create ssh key
 ssh-keygen -t rsa
